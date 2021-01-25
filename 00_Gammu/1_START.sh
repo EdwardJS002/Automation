c3840f1b3976de51a7c10d1af805d4daa3b6bcb2
edward <edwardjs002@gmail.com>
2021-01-25 15:14:36 +0000

    feat(script): merging both script

--- 00_Gammu/1_START.sh
+++ 00_Gammu/1_START.sh
@@ -6,34 +6,76 @@ if [ `whoami` != 'root' ]
     exit
 fi
 
+##############################################################################################
+
+#### Checking Version
+
+osversion=$(grep '^ID=' /etc/os-release | cut -c4-);
+
+echo $osversion
+
+if [ $osversion = 'ubuntu' ]
+  then 
+    echo "Vous avez choisi Ubuntu"
+
+    mv /boot/firmware/config.txt /boot/firmware/config_old.txt
+    mv /boot/firmware/cmdline.txt /boot/firmware/cmdline_old.txt
+
+    cp ./UTILS/ubuntu/config.txt /boot/firmware/config.txt
+    cp ./UTILS/ubuntu/cmdline.txt /boot/firmware/cmdline.txt
+
+    echo "CHANGING FILE OK"
+
+elif [ $osversion == 'raspbian' ]
+  then
+    echo "Vous avez choisi Raspberry Pi OS"
+
+    mv /boot/config.txt /boot/config_old.txt
+    mv /boot/cmdline.txt /boot/cmdline_old.txt
+
+    cp ./UTILS/raspberry/config.txt /boot/config.txt
+    cp ./UTILS/raspberry/cmdline.txt /boot/cmdline.txt
+
+    echo "CHANGING FILE OK"
+
+else
+  echo "Le Système d'exploitation n'a pas été reconnu... Aucune action n'a été effectuée..."
+  exit
+fi
+
+##############################################################################################
+
 echo "Quel est le numero de telephone Test ? :"
 read phone
 
 GAMMU_PHONE=$phone
 
-echo "Quel est le message à envoyer ? : "
-read test_message
-
-GAMMU_TEST_MESSAGE=$test_message
+#echo "Quel est le message à envoyer ? : "
+#read test_message
+#GAMMU_TEST_MESSAGE=$test_message
+GAMMU_TEST_MESSAGE=`Tout fonctionne nickel à $(date +%H%M)`
 
 echo "Quel est le mot de passe de la base de donnee ? :"
 read mysql_password
 
 GAMMU_MYSQL_PASSWORD=$mysql_password
 
+#echo "Quel est le chemin de l'appareil SMS ? :"
+#read device_path
+#GAMMU_DEVICE_PATH=$device_path
+GAMMU_DEVICE_PATH="/dev/ttyS0"
+
+
 #Install Docker
-apt-get update && apt-get upgrade
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-usermod -aG docker ubuntu
-rm get-docker.sh
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sh get-docker.sh
+#usermod -aG docker ubuntu
+#rm get-docker.sh
 
 #Update and Install Software
 echo "Installing Primary Software"
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y vim curl git zsh
-echo "Starting Oh My Zsh"
-yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt-get install -y vim curl git
 
 #Install Gammu and Mysql 
 echo "Installing Gammu and Mysql"
@@ -43,7 +85,7 @@ sudo apt-get install -y mariadb-server gammu gammu-smsd
 echo "Configuring gammurc"
 
 echo "[gammu]
-device= /dev/ttyS0
+device= $GAMMU_DEVICE_PATH
 connection = at115200" > /etc/gammurc
 
 #Testing Gammu SMS
@@ -64,7 +106,7 @@ mysql -e "FLUSH PRIVILEGES;"
 echo "Configuring Gammu-Smsd Configuration File"
 
 echo "[gammu]
-device= /dev/ttyS0
+device= $GAMMU_DEVICE_PATH
 connection = at115200
 
 [smsd]
@@ -74,7 +116,9 @@ host = localhost
 user = smsd
 password = $GAMMU_MYSQL_PASSWORD
 pc = localhost
-database = smsd" > /etc/gammu-smsdrc
+database = smsd
+CommTimeout=5
+" > /etc/gammu-smsdrc
 
 #Initialization of SMSD Database
 echo "Initialization of SMSD Database"
@@ -91,4 +135,5 @@ sleep 3
 gammu-smsd-inject TEXT $GAMMU_PHONE -len 1 -text "$GAMMU_TEST_MESSAGE"
 
 
-echo "It's Finished !"
+echo "It's Finished ! - Rebooting"
+
