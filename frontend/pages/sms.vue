67f27633e9c0de88b3a3e8cf2e5dcd1453d89b79
EdwardJS002 <edwardjs002@gmail.com>
2021-02-13 00:48:33 +0100

    web

---
+++ frontend/pages/sms.vue
@@ -0,0 +1,40 @@
+<template>
+	<v-form ref="form" v-model="valid" lazy-validation>
+		<v-textarea name="numbers" label="Phone Numbers"> </v-textarea>
+
+		<v-textarea name="messages" label="Messages"> </v-textarea>
+
+		<v-btn :disabled="!valid" color="success" class="mr-4" @click="validate"> Envoyer </v-btn>
+
+		<v-btn class="mr-4" @click="fetchSomething"> Nettoyer </v-btn>
+	</v-form>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		valid: true
+	}),
+
+	methods: {
+		validate() {
+			this.$refs.form.validate()
+			this.$refs.form.reset()
+		},
+		reset() {
+			this.$refs.form.reset()
+		},
+		notif: () => ({
+			snackbar: false,
+			text: `SMS Envoyé`
+		}),
+		async fetchSomething() {
+			const ip = await this.$axios.$get('http://icanhazip.com')
+			this.ip = ip
+			console.log(ip)
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
