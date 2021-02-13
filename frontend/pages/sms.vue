1cf2c82c5c4fa533ff662897830322bd710e4d98
EdwardJS002 <edwardjs002@gmail.com>
2021-02-13 03:24:38 +0100

    feat(sms): validation

--- frontend/pages/sms.vue
+++ frontend/pages/sms.vue
@@ -1,13 +1,22 @@
 <template>
-	<v-form ref="form" v-model="valid" lazy-validation>
-		<v-textarea name="numbers" label="Phone Numbers" v-model="phones" required> </v-textarea>
+	<div>
+		<v-form ref="form" v-model="valid" lazy-validation>
+			<v-textarea name="numbers" label="Phone Numbers" v-model="phones" required> </v-textarea>
 
-		<v-textarea name="messages" label="Messages" v-model="message" required></v-textarea>
+			<v-textarea name="messages" label="Messages" v-model="message" required></v-textarea>
 
-		<v-btn :disabled="!valid" color="success" class="mr-4" @click="validate"> Envoyer </v-btn>
+			<v-btn :disabled="!valid" color="success" class="mr-4" @click="validate"> Envoyer </v-btn>
 
-		<v-btn class="mr-4" @click="fetchSomething"> Nettoyer </v-btn>
-	</v-form>
+			<v-btn class="mr-4" @click="reset"> Nettoyer </v-btn>
+		</v-form>
+
+		<div class="text-center ma-2">
+			<v-btn dark @click="snackbar = true"> Open Snackbar </v-btn>
+			<v-snackbar v-model="snackbar" :timeout="timeout" color="teal">
+				{{ snackbarText }}
+			</v-snackbar>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -15,22 +24,32 @@ export default {
 	data: () => ({
 		valid: false,
 		message: '',
-		phones: ''
+		phones: '',
+		snackbar: false,
+		snackbarText: '',
+		timeout: 2000
 	}),
 
 	methods: {
 		validate() {
 			this.$refs.form.validate()
-			this.fetchSomething()
+			this.sendSMS(this.message, this.phones)
 			this.$refs.form.reset()
 		},
 		reset() {
+			this.notify('Formulaire Nettoyé')
 			this.$refs.form.reset()
 		},
-		async fetchSomething(event) {
-			const ip = await this.$axios.$get(`/sms?phone=${this.phones}&message=${this.message}`)
-			this.ip = ip
-			console.log(ip)
+		async sendSMS(message, phones) {
+			const response = await this.$axios.$get(`/sms?phone=${phones}&message=${message}`)
+			this.response = response
+			console.log(response)
+			this.notify('Message Envoyé !')
+		},
+
+		async notify(text) {
+			this.snackbar = true
+			this.snackbarText = text
 		}
 	}
 }
